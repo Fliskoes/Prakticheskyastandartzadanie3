2d19cd8e13083af5425a84412d1fee261557de7c
Fliskoes <fili2018@yandex.ru>
2025-05-18 17:58:28 +0300

    Initial commit: add project structure

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,31 @@
+import org.example.BonusService;
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+public class BonusServiceTest {
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 50;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        // производим проверку (сравниваем ожидаемый и
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 1000;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        // производим проверку (сравниваем ожидаемый и
+        Assertions.assertEquals(expected, actual);
+    }
+}
